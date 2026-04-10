c988a178b61992d4cdf514574f79bbecbe06432c
Annie <75939205+AnnieBunny@users.noreply.github.com>
2026-04-10 16:32:55 +0300

    Fix error
    By registering POST route inside the main function and correctly applying CORS middleware and add error handling in handlers.go for better error responses and debugging

--- backend/handles.go
+++ backend/handles.go
@@ -6,23 +6,58 @@ import (
 )
 
 func GetProducts(w http.ResponseWriter, r *http.Request) {
-	rows, _ := DB.Query("SELECT id, name, price FROM products")
 
-	var products []map[string]interface{}
+    rows, err := DB.Query("SELECT id, name, price FROM products")
+    if err != nil {
+        http.Error(w, err.Error(), 500)
+        return
+    }
+    defer rows.Close()
 
-	for rows.Next() {
-		var id int
-		var name string
-		var price int
+    var products []Product
 
-		rows.Scan(&id, &name, &price)
+    
+    for rows.Next() {
+        var p Product
+        if err := rows.Scan(&p.ID, &p.Name, &p.Price); err != nil {
+            http.Error(w, err.Error(), 500) 
+            return
+        }
+        products = append(products, p)
+    }
 
-		products = append(products, map[string]interface{}{
-			"id":    id,
-			"name":  name,
-			"price": price,
-		})
+    if err := rows.Err(); err != nil {
+        http.Error(w, err.Error(), 500)
+        return
+    }
+
+    w.Header().Set("Content-Type", "application/json")
+    w.WriteHeader(http.StatusOK) // 200 OK
+    json.NewEncoder(w).Encode(products)
+}
+
+func CreateProduct(w http.ResponseWriter, r *http.Request) {
+	var p Product
+
+	err := json.NewDecoder(r.Body).Decode(&p)
+	if err != nil {
+		http.Error(w, "Invalid JSON", 400)
+		return
 	}
 
-	json.NewEncoder(w).Encode(products)
+	result, err := DB.Exec(
+		"INSERT INTO products (name, price) VALUES (?, ?)",
+		p.Name,
+		p.Price,
+	)
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+
+	id, _ := result.LastInsertId()
+	p.ID = int(id)
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(p)
 }
--- backend/main.go
+++ backend/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/go-chi/cors"
 	"github.com/go-chi/chi/v5"
 )
 
@@ -13,7 +14,16 @@ func main() {
 
 	r := chi.NewRouter()
 
+	
+	r.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"*"},
+		AllowedMethods:   []string{"GET", "POST", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type"},
+	}))
+
+	
 	r.Get("/products", GetProducts)
+	r.Post("/products", CreateProduct)
 
 	fmt.Println("Server running on :8080")
 	http.ListenAndServe(":8080", r)
